8e65b07ef88415fce91a00279aba2570d60a4909
ernestizic <ieifeanyichukwu@gmail.com>
2023-03-04 11:19:29 +0100

    style: dashboard style fix

--- src/components/layout/dashboardLayout/DashboardLayoutStyle.js
+++ src/components/layout/dashboardLayout/DashboardLayoutStyle.js
@@ -2,15 +2,9 @@ import styled from "styled-components";
 
 export const DashboardContainer = styled.div`
     width: 100vw;
-    height: 100vh;
-    overflow: hidden;
     
     .dashboard-screen{
-        /* border: 1px solid blue; */
-        /* position: relative; */
         margin-left: 240px;
-        max-height: 100vh;
-        overflow: auto;
         padding-bottom: 54px;
         @media only screen and (max-width: 768px) { 
             margin: 0;
