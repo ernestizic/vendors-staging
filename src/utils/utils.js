fc1104c89c6f08791eae454399db7e1cfa17ddda
Ernest Isaac <ieifeanyichukwu@gmail.com>
2023-03-10 07:43:25 +0100

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,2 @@
 // Vendor base URL
-export const base_url_vendors =
-	process.env.NODE_ENV === 'development'
-		? 'https://staging-vendors.giftly.me/api'
-		: 'https://vendors.giftly.me/api';
+export const base_url_vendors = 'https://staging-vendors.giftly.me/api'
